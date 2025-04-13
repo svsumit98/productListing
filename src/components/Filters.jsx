04cbc0febcb5d72bf0c8e9f65f75906e5159bf9e
Sumit Kumar Verma <svsumit9810@gmail.com>
2025-04-13 15:01:24 +0530

    Css changed!!

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -39,7 +39,7 @@ const Filters = ({
         ))}
       </div>
       <div>
-        <h3 className="font-semibold mb-2">Max Price: ${priceRange[1]}</h3>
+        <h3 className="font-semibold mb-2">Max Price: ₹{priceRange[1]}</h3>
         <input
           type="range"
           min="0"
